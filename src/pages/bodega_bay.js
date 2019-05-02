f77514fb20d65daf954fe9f1b6b6f600fefaf211
M094650 <lucas.selker@macys.com>
2019-05-01 19:10:10 -0700

    central coast and bodega

--- src/pages/bodega_bay.js
+++ src/pages/bodega_bay.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import tahiti from '../images/tahiti/tahiti.jpg';
+import bodega from '../images/bodegaBay/bodega_flowers.jpg';
 import { graphql } from "gatsby";
 import Img from 'gatsby-image';
 import postStyle from './postStyles.css';
@@ -10,13 +10,13 @@ import NewMap from '../components/newMap';
 const BodegaBayPost = (props) => (
   
    <div>
-	<header className={"masthead"} style={{height: '539px', marginTop: '55px', backgroundImage: `url(${tahiti})`, position: 'absolute', zIndex: '-1', width: '100%'}}>
+	<header className={"masthead"} style={{height: '539px', marginTop: '55px', backgroundImage: `url(${bodega})`, position: 'absolute', zIndex: '-1', width: '100%'}}>
        <div className={"container"}>
 	 			<div className={"row"}>
 	 				<div className={"col-lg-8 col-md-10 mx-auto"}>
 	 					<div className={"post-heading"}>
-							<h1 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}}>Mo'orea and Bora Bora</h1>
-	 						<h2 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}} className="subheading">French Polynesia</h2>
+							<h1 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}}>Bodega Bay</h1>
+	 						<h2 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}} className="subheading">California</h2>
 	 					</div>
 	 				</div>
 	 			</div>
@@ -24,20 +24,65 @@ const BodegaBayPost = (props) => (
      </header>
   <Layout>
     <SEO 
-      title="Your guide to Tahiti • Somewhere With The Selkers"
-      description='Follow us on our honeymoon to Disneyland, Moorea and Bora Bora'
-      keywords={['tahiti', 'disney','bora bora','french polynesia','moorea']}
-      img={tahiti}
+      title="Your guide to Bodega Bay • Somewhere With The Selkers"
+      description='Bodega Bay - Northern California... taking it easy'
+      keywords={['bodega bay', 'california','northern california','coast','west coast']}
+      img={bodega}
     />
     <article className={'postWrapper'}>
 			<div className={"container"}>
 			<div className={"row mx-auto"} >
-			<NewMap
-				cityList={[
-					{name: 'San Francisco', latitude: 37.7749, longitude:-122.4194 },
-					{name: 'Papeete', latitude: 17.5516, longitude: 149.5585 },
-			]}
-			/></div>
+			<div class="col-lg-8 col-md-10 col-xs-12 mx-auto">
+              <p>
+							Bodega Bay is only an hour and a half away from San Francisco, but it's one of the coziest and laid back weekend getaway's you can take from the city. Once you exit the highway, it's a beautiful drive through the countryside to get to the coast.
+							 We made a small stop close to the bay for fresh baked goods at <a href="http://wildflourbread.com/" target="blank">Wild Flour Bread</a>. They had an amazing selection of scones, breads, cinnamon rolls - all baked fresh daily in their brick wood fired oven. They are in a small location nestled off the back roads , but churn out up to 900 loaves a day! 
+							 The staff was super friendly as well, smiling while offering to let patrons who forgot cash pay on their next visit. They even have a beautiful garden in the back, perfect for a nice quiet stroll. Only slightly of of the way, but highly recommended. They only sell their goodies here, so make sure to go!
+							<br/>
+							<br/>
+								<Img style={{display:'block',marginLeft: 'auto', marginRight: 'auto', width: '75%'}} fluid={props.data.wildflour.childImageSharp.fluid} alt="wildflour" />
+								<span class="caption text-muted">Quite a selection at Wild Flour Bread</span>
+							
+							<br/>
+							From the bakery, we drove around the bay to the Bodega Head Trail. There were a few picnic tables set up near the parking area overlooking the ocean where we were able to sit and enjoy our fresh breakfast. 
+							After our picnic, we followed the trail along the cliffs. Beautiful wildflowers, rocky landscapes, and crashing waves made for a quintessential Northern California walk. 
+							It's a flat and easy path that's perfect for anyone. We looped back to the other end of the trail and gathered with a whale watching group, who were fully equipped with tripod binoculars, cameras, and seats. We hung around for a while, but didn't end up seeing any whales - maybe next time!
+							<br/>
+							<br/>
+							<Img fluid={props.data.bodegaHead.childImageSharp.fluid} alt="bodega head" />
+							<span class="caption text-muted">Bodega Head Trail</span>
+							<br/>
+							
+							<Img fluid={props.data.bodegaHead2.childImageSharp.fluid} alt="bodega head 2" />
+							<span class="caption text-muted">Cliffs of Bodega Head</span>
+							
+							Before heading to our hotel, we stopped for a some wine and snacks on the bay at <a href="http://www.gourmetaubay.com" target="blank">Gourmet Au Bay</a>, where they serve up wine flights on mini surf boards. 
+							We soaked up some sun and enjoyed some seafood on their outdoor patio. 
+							<br/>
+							<br/>
+							The rest of our afternoon was spent lounging (and getting sunburnt - yes I forgot the sunscreen...) by the hotel pool at the Bodega Bay Lodge. Their property sits on the edge of the bay, overlooking the ocean and wetlands of the bay. 
+							A huge infinity hot tub is perched right on the edge of the area, the best way to take in the view. The weather was wonderful while we were there, so we were able to spend some of the night in the hot tub as well. We even left our hotel balcony door cracked open overnight and let the sound of the waves and the fog horn lull us to sleep.
+							<br/>
+							<br/>
+							After a sit down breakfast at the hotel Sunday morning, we headed to the <a href="http://www.bbfishfest.org/" target="blank">Fishermans Festival</a>, the real reason for our trip. The Fisherman's Festival is held every year in Bodega Bay. 
+							It is run completely by the local community and everything goes toward supporting the local services like the Fire Department, Search and Rescue, etc. They gave a live demo of a Search and Rescue Helicopter pulling swimmers from the Bay and bringing them to shore. 
+							They also host the Wooden Boat challenge that features teams building wooden boats from scratch and without power tools within 3 hours and then racing them. As is tradition, the festival marks the opening of fishing season in the area. To kick it off, there is a boat parade that travels the length of the bay and then the annual blessing of the fleet.
+							<br/>
+							<br/>
+							<Img fluid={props.data.boatParade.childImageSharp.fluid} alt="boat parade" />
+							<span class="caption text-muted">Fishermans Festival Boat Parade</span>
+							<br/>
+							
+							<Img fluid={props.data.fireTruck.childImageSharp.fluid} alt="fire truck" />
+							<span class="caption text-muted">Bodega Bay Fire Truck</span>
+
+
+
+                        </p>
+						<h4>Accomodations</h4>
+						<a href="https://bodegabaylodge.com/" target="blank">
+							<i class="fa fa-bed" aria-hidden="true"></i> Bodega Bay Lodge</a>
+					</div>
+			</div>
 			</div>
 		</article>
   </Layout>
@@ -51,63 +96,49 @@ export default BodegaBayPost;
 
 export const pageQuery = graphql`
   query {
-    atv1: file(relativePath: { eq: "tahiti/atv1.jpg" }) {
+    boatParade: file(relativePath: { eq: "bodegaBay/boat_parade.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
         }
       }
 	}
-	atv2: file(relativePath: { eq: "tahiti/atv2.jpg" }) {
+	bodegaFlowers: file(relativePath: { eq: "bodegaBay/bodega_flowers.jpg" }) {
 		childImageSharp {
 		  fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 		  }
 		}
 	  }
-	catamaran: file(relativePath: { eq: "tahiti/catamaran.jpg" }) {
+	bodegaHead: file(relativePath: { eq: "bodegaBay/bodega_head.jpg" }) {
 	childImageSharp {
 		fluid(maxWidth: 1000) {
 		...GatsbyImageSharpFluid
 		}
 	  }
 	}
-	fish: file(relativePath: { eq: "tahiti/fish.jpg" }) {
+	bodegaHead2: file(relativePath: { eq: "bodegaBay/bodega_head2.jpg" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 			}
 		  }
 		}
-	hut: file(relativePath: { eq: "tahiti/hut.jpg" }) {
+	fireTruck: file(relativePath: { eq: "bodegaBay/fire_truck.jpg" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 			}
 		  }
 		}
-	moorea_hike: file(relativePath: { eq: "tahiti/moorea_hike.jpg" }) {
+	surfBoard: file(relativePath: { eq: "bodegaBay/surf_board.JPG" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 			}
 		  }
 		}
-	moorea_pool: file(relativePath: { eq: "tahiti/moorea_pool.jpg" }) {
-		childImageSharp {
-			fluid(maxWidth: 1000) {
-			...GatsbyImageSharpFluid
-			}
-		  }
-		}
-	mountain: file(relativePath: { eq: "tahiti/mountain.jpg" }) {
-		childImageSharp {
-			fluid(maxWidth: 1000) {
-			...GatsbyImageSharpFluid
-			}
-		  }
-		}
-	tahiti: file(relativePath: { eq: "tahiti/tahiti.jpg" }) {
+	wildflour: file(relativePath: { eq: "bodegaBay/wildflour.JPG" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
--- src/pages/central_coast.js
+++ src/pages/central_coast.js
@@ -1,22 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import tahiti from '../images/tahiti/tahiti.jpg';
+import pismo from '../images/centralCoast/pismoBeach.jpg';
 import { graphql } from "gatsby";
 import Img from 'gatsby-image';
 import postStyle from './postStyles.css';
-import NewMap from '../components/newMap';
+import { FaClock } from 'react-icons/fa';
+
 
 const CentralCoastPost = (props) => (
   
    <div>
-	<header className={"masthead"} style={{height: '539px', marginTop: '55px', backgroundImage: `url(${tahiti})`, position: 'absolute', zIndex: '-1', width: '100%'}}>
+	<header className={"masthead"} style={{height: '539px', marginTop: '55px', backgroundImage: `url(${pismo})`, position: 'absolute', zIndex: '-1', width: '100%'}}>
        <div className={"container"}>
 	 			<div className={"row"}>
 	 				<div className={"col-lg-8 col-md-10 mx-auto"}>
 	 					<div className={"post-heading"}>
-							<h1 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}}>Mo'orea and Bora Bora</h1>
-	 						<h2 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}} className="subheading">French Polynesia</h2>
+							<h1 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}}>Central Coast</h1>
+	 						<h2 style={{fontFamily: 'Pacifico', fontWeight: '500', textAlign: 'center'}} className="subheading">California</h2>
 	 					</div>
 	 				</div>
 	 			</div>
@@ -24,20 +25,123 @@ const CentralCoastPost = (props) => (
      </header>
   <Layout>
     <SEO 
-      title="Your guide to Tahiti • Somewhere With The Selkers"
-      description='Follow us on our honeymoon to Disneyland, Moorea and Bora Bora'
-      keywords={['tahiti', 'disney','bora bora','french polynesia','moorea']}
-      img={tahiti}
+      title="Weekend Guide to California's Central Coast • Somewhere With The Selkers"
+      description='Follow us to the coast of central Cali for some fun in the sun'
+      keywords={['california', 'central coast','pismo beach','avila beach','bob jones trail', 'san luis obispo']}
+      img={pismo}
     />
     <article className={'postWrapper'}>
 			<div className={"container"}>
 			<div className={"row mx-auto"} >
-			<NewMap
-				cityList={[
-					{name: 'San Francisco', latitude: 37.7749, longitude:-122.4194 },
-					{name: 'Papeete', latitude: 17.5516, longitude: 149.5585 },
-			]}
-			/></div>
+			<div className={"col-lg-8 col-md-10 col-xs-12 mx-auto"}>
+						<p>
+						<FaClock style={{color: 'gray'}}/><span style ={{fontStyle: 'italic', color: 'gray'}}> 2 min read </span><br/><br/>
+							The Central Coast of California between San Francisco and Los Angeles is dotted with some of the best beach towns in the
+							country. We took off on a Friday after work and were down the coast to San Luis Obispo in a matter of hours. We spent
+							Friday night at the Sycamore Mineral Springs Resort and Spa, where we treated ourselves to an hour in one the private
+							hillside hot tubs and a bottle of wine. On Saturday morning, we rented bikes from the resort and followed the
+							<a
+							 href="http://www.avilabeachpier.com/information/bob-jones-bike-trail.html" target="blank">Bob Jones Trail</a> to Avila Beach. It's a great casual trail, taking you along a stream and dropping you out right
+								on the sand. We could have easily spent an entire day at Avila Beach. There are several bars and shops right along
+								the beach stretch, perfect for beaching with minimal effort.
+								<br/>
+								<br/>
+								<Img fluid={props.data.bobJonesTrail.childImageSharp.fluid} alt="bob jones trail" />
+								<span className={"caption text-muted"}>Bob Jones Trail</span>
+								<br/>
+
+								<Img fluid={props.data.avilaBeach.childImageSharp.fluid} alt="avila beach" />
+								<span className="caption text-muted">Avila Beach Shops</span>
+
+								There are quite a few wineries near San Luis Obispo, and the warm weather makes for perfect picnicking. We stopped at three
+								wineries during the afternoon, all within a short drive of each other:
+								<ul>
+									<li>
+										<strong>
+											<a href="http://www.biddleranch.com/About/Tasting-Room" style={{textDecoration: 'underline'}} target="blank">Biddle Ranch</a>
+										</strong> had an intimate wine tasting room as well as a large outdoor area full of tables and even cornhole.
+									</li>
+									<br/>
+									<li>
+										<strong>
+											<a href="http://www.ednavalleyvineyard.com/visit/tasting-room" style={{textDecoration: 'underline'}} target="blank">Edna</a>
+										</strong> winery was larger, but very elegant. We stayed here for most of the afternoon with the picnic we packed and a bottle
+										of wine.
+									</li>
+									<br/>
+									<li>
+										<strong>
+											<a href="http://baileyana.com/taste/" style={{textDecoration: 'underline'}} target="blank">Baileyana</a>
+										</strong> was an interesting tasting room because it is housed in a schoolhouse built in 1909. They were the busiest of the
+										three we visited, but also had tons of outdoor space for relaxing.
+									</li>
+								</ul>
+								<br/>
+								<Img fluid={props.data.biddle.childImageSharp.fluid} alt="biddle winery" />
+								<span className={"caption text-muted"}>Biddle Ranch Vineyard</span>
+								<br/>
+
+								It's a quick drive from the vineyards back to the beach. Our second night we stayed in the Sea Cliff Hotel, with an amazing
+								balcony view overlooking the ocean cliffs and
+								Pismo Beach. There are alot of hotels lined along the cliffs. In the morning, we walked along the trail connecting
+								them all, down a long set of stairs and across sandy rocks to get to the beach. There's only one real way to start
+								your day on Pismo Beach - getting breakfast at
+								<a href="https://oldwestcinnamonrolls.com/" style={{textDecoration: 'underline'}}
+								 target="blank">Old West Cinnamon Rolls</a>.
+								<br/>
+								<br/>
+
+								<div className={"row"}>
+									<div className={"col-6 mx-auto"}>
+										<Img fluid={props.data.trail.childImageSharp.fluid} alt="trail" />
+										<span className={"caption text-muted"}>Cliff Trail</span>
+
+									</div>
+									<div className={"col-6 mx-auto"}>
+										<Img fluid={props.data.rockStairs.childImageSharp.fluid} alt="rock stairs" />
+										<span className={"caption text-muted"}>Stairs to Pismo</span>
+									</div>
+								</div>
+
+								<h4>Food</h4>
+								<br/>
+								After walking around downtown San Luis Obispo, we went to dinner at
+								<a href="http://www.lunaredslo.com/" style={{textDecoration: 'underline'}}
+								 target="blank">Luna Red</a>. The restaurant sits right along a historic mission and has a beautiful outdoor patio and bar with strings
+								of lights. We loved the bacon wrapped dates and the chicken paella.
+								<br/>
+								<br/>
+								In Pismo Beach, we stopped at
+								<a href="http://www.crackedcrab.com/" style={{textDecoration: 'underline'}} target="blank">Cracked Crab</a>. We strapped on bibs and dove into the bucket of crab, potatoes, and corn dumped in front of us.
+								It's good for the soul to have a delicious messy meal every once and a while.
+								<br/>
+								<br/>
+								Before we got back on the road to San Francisco, we made one final stop at
+								<a href="http://www.slodoco.com/" style={{textDecoration: 'underline'}}
+								 target="blank">SloDoCo Donuts</a> for road snacks and a couple dozen to share. Open 24 hours a day, they have a donut for everyone.
+								You can get Nutella Pillows, donuts with breakfast cereal toppings, and the famous Galaxy donut.<br/>
+								<br/>
+
+								<div className={"row"}>
+									<div className={"col-6 mx-auto"}>
+										<Img fluid={props.data.crackedCrab.childImageSharp.fluid} alt="cracked crab" />
+										<span className={"caption text-muted"}>Cracked Crab</span>
+
+									</div>
+									<div className={"col-6 mx-auto "}>
+										<Img fluid={props.data.slodoco.childImageSharp.fluid} alt="Slo Donut Company" />
+										<span className={"caption text-muted"}>Slo Donut Co</span>
+									</div>
+								</div>
+						</p>
+						<h4>Accomodations</h4>
+						<a href="https://www.sycamoresprings.com/">
+							<i className="fa fa-bed" aria-hidden="true"></i> Sycamore Mineral Springs Resort and Spa</a>
+						<br/>
+						<a href="https://www.shorecliff.com/">
+							<i className="fa fa-bed" aria-hidden="true"></i> Shore Cliff Hotel</a>
+					</div>
+			</div>
 			</div>
 		</article>
   </Layout>
@@ -51,63 +155,49 @@ export default CentralCoastPost;
 
 export const pageQuery = graphql`
   query {
-    atv1: file(relativePath: { eq: "tahiti/atv1.jpg" }) {
+    avilaBeach: file(relativePath: { eq: "centralCoast/avila_beach.JPG" }) {
       childImageSharp {
         fluid(maxWidth: 1000) {
           ...GatsbyImageSharpFluid
         }
       }
 	}
-	atv2: file(relativePath: { eq: "tahiti/atv2.jpg" }) {
+	biddle: file(relativePath: { eq: "centralCoast/biddle.JPG" }) {
 		childImageSharp {
 		  fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 		  }
 		}
 	  }
-	catamaran: file(relativePath: { eq: "tahiti/catamaran.jpg" }) {
+	bobJonesTrail: file(relativePath: { eq: "centralCoast/bob_jones_trail.JPG" }) {
 	childImageSharp {
 		fluid(maxWidth: 1000) {
 		...GatsbyImageSharpFluid
 		}
 	  }
 	}
-	fish: file(relativePath: { eq: "tahiti/fish.jpg" }) {
+	crackedCrab: file(relativePath: { eq: "centralCoast/cracked_crab.jpg" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 			}
 		  }
 		}
-	hut: file(relativePath: { eq: "tahiti/hut.jpg" }) {
+	rockStairs: file(relativePath: { eq: "centralCoast/rock_stairs.JPG" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 			}
 		  }
 		}
-	moorea_hike: file(relativePath: { eq: "tahiti/moorea_hike.jpg" }) {
+	slodoco: file(relativePath: { eq: "centralCoast/slodoco.jpg" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
 			}
 		  }
 		}
-	moorea_pool: file(relativePath: { eq: "tahiti/moorea_pool.jpg" }) {
-		childImageSharp {
-			fluid(maxWidth: 1000) {
-			...GatsbyImageSharpFluid
-			}
-		  }
-		}
-	mountain: file(relativePath: { eq: "tahiti/mountain.jpg" }) {
-		childImageSharp {
-			fluid(maxWidth: 1000) {
-			...GatsbyImageSharpFluid
-			}
-		  }
-		}
-	tahiti: file(relativePath: { eq: "tahiti/tahiti.jpg" }) {
+	trail: file(relativePath: { eq: "centralCoast/trail.JPG" }) {
 		childImageSharp {
 			fluid(maxWidth: 1000) {
 			...GatsbyImageSharpFluid
